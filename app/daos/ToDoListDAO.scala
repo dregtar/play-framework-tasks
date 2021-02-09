67bdfd922bfd70eae04d2e6afce288a881dc481e
Vyacheslav Tyan <tyan@kinoplan.ru>
2021-02-09 14:23:24 +0300

    DAO, services, model, route formatting

--- app/daos/ToDoListDAO.scala
+++ app/daos/ToDoListDAO.scala
@@ -1,27 +1,31 @@
 package daos
 
 import javax.inject.Inject
-
-import models.Task
+import scala.concurrent.{ExecutionContext, Future}
 
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.JdbcProfile
 import slick.jdbc.MySQLProfile.api._
+import slick.lifted.ProvenShape
 
-import scala.concurrent.{ExecutionContext, Future}
+import models.Task
 
 
-class TasksTableDef(tag: Tag) extends Table[Task](tag, "tasks")
-{
-  def id = column[Long]("id", O.PrimaryKey,O.AutoInc)
-  def description = column[String]("description")
-  def isActive = column[Boolean]("is_active")
+class TasksTableDef(tag: Tag) extends Table[Task](tag, "tasks") {
 
-  override def * =
-    (id, description, isActive) <> ( (Task.apply _).tupled, Task.unapply)
+  def id: Rep[Long] = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+  def description: Rep[String] = column[String]("description")
+
+  def isActive: Rep[Boolean] = column[Boolean]("is_active")
+
+  override def * : ProvenShape[Task] =
+    (id, description, isActive) <> ((Task.apply _).tupled, Task.unapply)
 }
 
-class Tasks @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
+class Tasks @Inject()(
+  protected val dbConfigProvider: DatabaseConfigProvider)(
+  implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
 
   val tasks = TableQuery[TasksTableDef]
 
@@ -30,17 +34,14 @@ class Tasks @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(i
       dbConfig.db.run(tasks.filter(_.isActive).result)
     else
       dbConfig.db.run(tasks.result)
-
   }
 
   def get(id: Long): Future[Option[Task]] = {
     dbConfig.db.run(tasks.filter(_.id === id).result.headOption)
   }
 
-  def add(task: Task): Future[String] = {
-    dbConfig.db.run(tasks+=task).map(res => "Task successfully added")recover {
-            case ex: Exception => ex.getCause.getMessage
-     }
+  def add(task: Task): Future[String] = dbConfig.db.run(tasks += task).map(res => "Task successfully added") recover {
+    case ex: Exception => ex.getCause.getMessage
   }
 
   def update(task: Task): Future[Int] = {
@@ -55,20 +56,13 @@ class Tasks @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(i
     dbConfig.db.run(tasks.filter(!_.isActive).delete)
   }
 
-  def resetStatus(id : Long, isActive: Boolean): Future[Int] = {
-    val q = for { t <- tasks if t.id === id} yield t.isActive
+  def resetStatus(id: Long, isActive: Boolean): Future[Int] = {
+    val q = for {t <- tasks if t.id === id} yield t.isActive
     dbConfig.db.run(q.update(isActive))
   }
 
   def resetAllStatuses(isActive: Boolean): Future[Int] = {
-    val q = for { t <- tasks} yield t.isActive
+    val q = for {t <- tasks} yield t.isActive
     dbConfig.db.run(q.update(isActive))
   }
-
-
-
-
-
-
-
 }
