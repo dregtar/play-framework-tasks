319c4216909ffebeb04bba12c77c37597a4455f3
Vyacheslav Tyan <tyan@kinoplan.ru>
2021-02-09 13:09:46 +0300

    Code was formatted as defined in KP Code Style. Imports are optimized

--- app/controllers/ToDoListController.scala
+++ app/controllers/ToDoListController.scala
@@ -1,61 +1,53 @@
 package controllers
 
-import models.Task
-
 import javax.inject._
-import play.api.libs.json.Json
-import play.api.mvc._
-import services.ToDoListServices
 
 import scala.concurrent.ExecutionContext
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-@Singleton
-class ToDoListController @Inject()(toDoListServices: ToDoListServices)(implicit ec: ExecutionContext) extends InjectedController {
+import play.api.libs.json.Json
+import play.api.mvc._
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+import models.Task
+import services.ToDoListServices
+
+@Singleton
+class ToDoListController @Inject()(
+  toDoListServices: ToDoListServices)
+  (implicit ec: ExecutionContext) extends InjectedController {
+
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
-  def allTasks(onlyActive: Boolean) = Action.async {
+  def allTasks(onlyActive: Boolean): Action[AnyContent] = Action.async {
     toDoListServices.listAll(onlyActive).map(tasks => Ok(Json.toJson(tasks)))
   }
 
-  def getTask(id: Long) = Action.async {
+  def getTask(id: Long): Action[AnyContent] = Action.async {
     toDoListServices.getTask(id).map(tasks => Ok(Json.toJson(tasks)))
   }
 
-  def addTask(description: String) = Action.async { _ =>
+  def addTask(description: String): Action[AnyContent] = Action.async {
     toDoListServices.addTask(Task(description = description)).map(_ => NoContent)
   }
 
-  def updateTask(id: Long, description: String, isActive: Boolean) = Action.async { _ =>
+  def updateTask(id: Long, description: String, isActive: Boolean): Action[AnyContent] = Action.async {
     toDoListServices.updateTask(Task(id, description, isActive)).map(_ => NoContent)
   }
 
-  def resetStatus(id: Long, isActive: Boolean) = Action.async { _ =>
-    toDoListServices.resetStatus(id,isActive).map(_ => NoContent)
+  def resetStatus(id: Long, isActive: Boolean): Action[AnyContent] = Action.async {
+    toDoListServices.resetStatus(id, isActive).map(_ => NoContent)
   }
 
-  def resetAllStatuses(isActive: Boolean) = Action.async { _ =>
+  def resetAllStatuses(isActive: Boolean): Action[AnyContent] = Action.async {
     toDoListServices.resetAllStatuses(isActive).map(_ => NoContent)
   }
 
-  def deleteTask(id: Long) = Action.async { _ =>
+  def deleteTask(id: Long): Action[AnyContent] = Action.async {
     toDoListServices.deleteTask(id).map(_ => NoContent)
   }
 
-  def deleteInactive() = Action.async { _ =>
-    toDoListServices.deleteInactive.map(_ => NoContent)
+  def deleteInactive(): Action[AnyContent] = Action.async {
+    toDoListServices.deleteInactive().map(_ => NoContent)
   }
 }
